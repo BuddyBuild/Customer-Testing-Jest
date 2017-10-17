cd75f64a0c41fe19621a5f48f2c630acd518fdb6
Christopher Stott <christopherstott@gmail.com>
2017-10-17 02:53:09 +0100

    fix

--- buddybuild_prebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Jest won't create our folder structure for us, so lets create it first
-mkdir buddybuild_artifacts && mkdir buddybuild_artifacts/Jest
-
-if [[ "$BUDDYBUILD_BUILD_NUMBER" -ne "1" ]]; then
-  yarn run jest --outputFile=buddybuild_artifacts/Jest/jest.json --json
-fi
